5cd4c794f0cff2382dc368a69914d90c6cc8422b
Your Name <you@example.com>
2024-06-13 10:50:05 +0700

    example7 polygon

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_animation/examples/example3.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,6 +24,45 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Polygon extends CustomPainter {
+  final int sides;
+  Polygon({required this.sides});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 3;
+    final path = Path();
+    final center = Offset(size.width / 2, size.height / 2);
+    final angle = (2 * pi) / sides;
+    final angles = List.generate(sides, (index) => angle * index);
+    final radius = size.width / 2;
+
+    path.moveTo(
+      center.dx + radius * cos(0),
+      center.dy + radius * sin(0),
+    );
+
+    for (final angle in angles) {
+      path.lineTo(
+        center.dx + radius * cos(angle),
+        center.dy + radius * sin(angle),
+      );
+    }
+
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) =>
+      oldDelegate is Polygon && oldDelegate.sides != sides;
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -29,15 +70,97 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late AnimationController _sidesController;
+  late AnimationController _radiusController;
+  late AnimationController _rotationController;
+  late Animation _sideAnimation;
+  late Animation _radiusAnimation;
+  late Animation _rotationAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _sidesController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _sideAnimation = IntTween(
+      begin: 3,
+      end: 10,
+    ).animate(_sidesController);
+
+    _radiusController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _radiusAnimation = Tween(
+      begin: 20.0,
+      end: 400.0,
+    )
+        .chain(
+          CurveTween(curve: Curves.bounceInOut),
+        )
+        .animate(_radiusController);
+
+    _rotationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _rotationAnimation = Tween(
+      begin: 0.0,
+      end: 2 * pi,
+    )
+        .chain(
+          CurveTween(curve: Curves.easeInOut),
+        )
+        .animate(_rotationController);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _sidesController.repeat(reverse: true);
+    _radiusController.repeat(reverse: true);
+    _rotationController.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _sidesController.dispose();
+    _radiusController.dispose();
+    _rotationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Animation'),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: Listenable.merge([
+            _sidesController,
+            _radiusController,
+            _rotationController,
+          ]),
+          builder: (context, child) {
+            return Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()
+                ..rotateX(_rotationAnimation.value)
+                ..rotateY(_rotationAnimation.value)
+                ..rotateZ(_rotationAnimation.value),
+              child: CustomPaint(
+                painter: Polygon(sides: _sideAnimation.value),
+                child: SizedBox(
+                  width: _radiusAnimation.value,
+                  height: _radiusAnimation.value,
+                ),
+              ),
+            );
+          },
+        ),
       ),
-      body: const Example3(),
     );
   }
 }
